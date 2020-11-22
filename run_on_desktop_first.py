a1c942cfbe3bf439b84e725aa86429cfbab35fe4
sadunhaq123 <65484025+sadunhaq123@users.noreply.github.com>
2020-11-22 05:38:33 -0600

    Update run_on_desktop_first.py

--- run_on_desktop_first.py
+++ run_on_desktop_first.py
@@ -27,8 +27,12 @@ data=[]
 print ('Device:',addr)
 while 1:
     dictionary_with_partition_and_input = conn.recv(BUFFER_SIZE)
+    #print("AAA")
+    #print(dictionary_with_partition_and_input)
+    #print(type(dictionary_with_partition_and_input))
     if not dictionary_with_partition_and_input:
         break
+    #data.append(b"".join(bytes(dictionary_with_partition_and_input)))
     data.append(dictionary_with_partition_and_input)
 
 dictionary_with_partition_and_input = pickle.loads(b"".join(data))
@@ -41,7 +45,7 @@ conn.close()
 
 def server_call(model, inputs, partition_point):
     count = 0
-    for layer in model.layers[0: partition_point]:
+    for layer in model.layers[partition_point:]:
         inputs = layer(inputs)
         print("Layer ", layer)
         print(count)
@@ -55,21 +59,24 @@ def run_test_harness():
     # load model
     model = load_model('final_model.h5')
 
+    #model = load_model('final_model_custom_split.h5')
     model.summary()
     y_tensor_send = server_call(model, input_data, partition_point)
     y_tensor_send = y_tensor_send.numpy()
+    #final_output_send = final_outputs.numpy()
     print("Sending")
 
 
 
     #Run the data in the partition from 0 to given value, and send the results to the Pi.
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #s.connect((TCP_IP3, PORT4))
-    s.connect((TCP_IP2, PORT4))
+    s.connect((TCP_IP3, PORT4))
+    #s.connect((TCP_IP2, PORT4))
 
 
     data_final = pickle.dumps(y_tensor_send, protocol=pickle.HIGHEST_PROTOCOL)
 
+    # time.sleep(1)
     s.sendall(data_final)
 
     s.close()
